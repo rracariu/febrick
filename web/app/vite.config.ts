a0707746a93f40cfdd6bd4e5ba8c8e25b70c09ec
Radu <radu.racariu@gmail.com>
2024-03-24 19:48:39 -0700

    Try to fix the brick path

--- web/app/vite.config.ts
+++ web/app/vite.config.ts
@@ -3,11 +3,13 @@ import { defineConfig } from 'vite';
 import wasm from 'vite-plugin-wasm';
 import path from 'node:path';
 
+console.log('Brick location:', path.resolve(__dirname, '../../Brick.ttl'));
+
 export default defineConfig({
 	plugins: [sveltekit(), wasm()],
 	resolve: {
 		alias: {
-			'@brick': path.resolve(__dirname, '../../Brick.ttl?raw')
+			'@brick': path.resolve(__dirname, '../../Brick.ttl') + '?raw'
 		}
 	},
 	build: {
